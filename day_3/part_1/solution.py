e20707e6b6f80f2f64e966bf770cacee5afcc857
Marcin Nowak <freepizzas#4224>
2021-12-05 22:47:28 -0500

    improved comments

--- day_3/part_1/solution.py
+++ day_3/part_1/solution.py
@@ -1,11 +1,13 @@
 import os
 
+#count the number of '1's in each bit; if it's greater than half the length,
+#it's the 'most common bit' for that position
 line_count = 0
 bit_counts = []
 with open("input.txt", "r", encoding='utf-8') as input:
     data = input.readlines()
     for line in data:
-        line = line.strip()
+        line = line.strip() #strip newline character at end of each line
         line_count += 1
         index = 0
         for letter in line:
@@ -35,8 +37,8 @@ for bit in bit_counts:
 epsilon_rate = int(epsilon_string, 2)
 gamma_rate = int(gamma_string, 2)
 
-print('epsilon: ' + str(epsilon_string))
-print('epsilon: ' + str(epsilon_rate))
-print('gamma: ' + str(gamma_string))
-print('gamma: ' + str(gamma_rate))
+print('epsilon(2): ' + str(epsilon_string))
+print('gamma(2): ' + str(gamma_string))
+print('epsilon(10): ' + str(epsilon_rate))
+print('gamma(10): ' + str(gamma_rate))
 print('power: ' + str(epsilon_rate * gamma_rate))
