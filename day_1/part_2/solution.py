7fcf2bf6e00c610ff17555430be5555475142dcd
Marcin Nowak <freepizzas#4224>
2021-12-05 21:48:44 -0500

    day 1 part 2

---
+++ day_1/part_2/solution.py
@@ -0,0 +1,20 @@
+import os
+
+measurements = []
+with open("input.txt", "r", encoding='utf-8') as input:
+    input_lines = input.readlines()
+    for line in input_lines:
+        measurements.append(int(line))
+
+index = 0
+previous_window = None
+increase_count = 0
+while index < (len(measurements) - 2):
+    window_total = measurements[index] + measurements[index + 1] + measurements[index + 2]
+    if previous_window != None:
+        if window_total > previous_window:
+            increase_count += 1
+    previous_window = window_total
+    index += 1
+
+print(increase_count)
